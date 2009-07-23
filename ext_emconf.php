b66cfa3fe26a24e0d3915726728a4be5179e2ea3
dennisg <dennisg@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-23 14:00:45 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dg_keywordmenu/trunk@22721 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,43 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "dg_keywordmenu"
+#
+# Auto generated 23-07-2009 15:09
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Keywordmenu',
+	'description' => 'Description',
+	'category' => 'plugin',
+	'author' => 'Dennis Grote',
+	'author_email' => 'd.grote@dd-medien.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
