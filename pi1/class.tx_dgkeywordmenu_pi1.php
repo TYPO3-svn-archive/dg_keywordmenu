c34c305e0046b6de628b8e612337f908fc6ba173
dennisg <dennisg@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-23 12:58:25 +0000

    add ability for "All" link
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dg_keywordmenu/trunk@24739 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_dgkeywordmenu_pi1.php
+++ pi1/class.tx_dgkeywordmenu_pi1.php
@@ -80,8 +80,12 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 				 
 			case 'BOTH':
 				$content .= $this->display_menu();
-				$content .= $this->display_list_headline();
-				$content .= $this->display_list();
+				if ($this->currentLetter == 'ALL') {
+					$content .= $this->display_list_all();
+				} else {
+					$content .= $this->display_list_headline();
+					$content .= $this->display_list();
+				}
 				break;
 				 
 			default:
@@ -146,7 +150,7 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 		if ($this->theCode == 'BOTH') $this->conf['singlePid'] = $GLOBALS['TSFE']->id;
 
 		// load GET parameter
-		$this->currentLetter = htmlspecialchars($this->piVars['letter']);
+		$this->currentLetter = htmlspecialchars(strtoupper($this->piVars['letter']));
 		if ($this->theCode == 'BOTH' && $this->currentLetter == '') $this->currentLetter = 'A';
 
 	}
@@ -159,9 +163,11 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 	function display_list_headline() {
 		// Read in the part of the template file for keyword listing
 		$template = $this->cObj->getSubpart($this->tmpl, '###TEMPLATE_KEYWORD_LIST_HEADLINE###');
+		
 		// Get subpart template
 		$subTemplate = $this->cObj->getSubpart($template, '###HEADLINE###');
 
+		// Substitute marker
 		$subPartContent = $this->cObj->substituteMarker($subTemplate, '###HEADLINELETTER###', $this->currentLetter);
 
 		// Substitute subpart
@@ -178,6 +184,7 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 	function display_list() {
 		// Read in the part of the template file for keyword listing
 		$template = $this->cObj->getSubpart($this->tmpl, '###TEMPLATE_KEYWORD_LIST###');
+		
 		// Get subpart template
 		$subTemplate = $this->cObj->getSubpart($template, '###KEYWORDS###');
 
@@ -231,6 +238,8 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 
 		// get keywords from database and put it in marker
 		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			
+			// conf array for the typolink
 			$typolink_conf = array(
             	'title' => $row['keyword'],
             	'no_cache' => 0,
@@ -238,6 +247,8 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
            		'additionalParams' => '');
 			 
 			$listItem = $this->cObj->typolink($row['keyword'], $typolink_conf);
+			
+			// Substitute marker
 			$subPartContent .= $this->cObj->substituteMarker($subTemplate, '###KEYWORD###', $listItem);
 		}
 
@@ -248,6 +259,91 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 
 		return $content;
 	}
+	
+	
+/**
+	 * display keywordlist all
+	 *
+	 * @return	html code of the keywordlist all
+	 */
+	function display_list_all() {
+
+		// query mit inner join nach der alten schreibweise
+		$select = 'T1.keyword, T1.link';
+		$from = 'tx_dgkeywordmenu_keywords T1, pages T2';
+		$where = 'T1.link=T2.uid AND T1.deleted = 0 AND T1.hidden = 0 AND T2.deleted = 0 AND T2.hidden = 0 AND T1.pid = '.$this->pidList.'';
+		$groupBy = '';
+		$orderBy = 'T1.keyword';
+		$limit = '';
+
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select, $from, $where, $groupBy, $orderBy, $limit);
+
+		// get keywords from database and put it in array
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			$simplifiedKeyword = $this->simplifyString($row['keyword']);
+			
+			// generate key from keyword (i.e. 'test' => 'T')
+			$key = strtoupper(substr($simplifiedKeyword, 0, 1));
+			
+			// the keyword array
+			$listItems[$key][$simplifiedKeyword] = $row['link'];
+			$originalKeywords[$simplifiedKeyword] = $row['keyword'];
+		}
+		//t3lib_div::debug($listItems,listItem);
+		//t3lib_div::debug($originalKeywords,origKeywords);
+		
+		
+		
+		// Loop through the keys
+		foreach ($listItems AS $key => $items) {
+
+			// header for the current section
+			$sectionHeader = '';
+
+			// get first character
+			$firstchar = $key;
+
+			// If a new char appears, create new section
+			if ($firstchar != $lastchar) {
+
+				// add sectionHeader (capital letter)
+				$sectionHeader .= $this->cObj->wrap($firstchar, $this->conf['sectionHeaderWrap']);
+
+				// add character to existing char list
+				$this->existingKeys[] = $firstchar;
+
+				// set vars for next loop
+				$lastchar = $firstchar;
+			}
+
+			$keywordList = '';
+			$prevKeyword = '';
+
+			foreach ($items AS $keyword => $link) {
+				if ($keyword != $prevKeyword) {
+					
+					// conf array for the typolink
+					$typolink_conf = array(
+            			'title' => $originalKeywords[$keyword],
+            			'no_cache' => 0,
+          			  	'parameter' => $link,
+         		  		'additionalParams' => '');
+					
+					// make link from keyword
+					$keywordList .= $this->cObj->wrap($this->cObj->typolink($originalKeywords[$keyword], $typolink_conf), $this->conf['keywordListItemWrap']);
+				}
+				
+				// set vars for next loop
+				$prevKeyword = $keyword;
+			}
+
+			$keywordList = $this->cObj->wrap($keywordList, $this->conf['keywordListWrap']);
+			$keywordSection = $this->cObj->wrap($keywordList, $this->conf['keywordSectionWrap']);
+			$content .= $sectionHeader . $keywordSection;
+		}
+		
+		return $content;
+	}
 	 
 	 
 	/**
@@ -256,6 +352,9 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 	 * @return	html code of the a-z menu
 	 */
 	function display_menu() {
+		// add 'ALL' to $indexKeys
+		if ($this->conf['enableAllLink'] == '1') $this->conf['indexKeys'] = 'ALL,'.$this->conf['indexKeys'];
+		
 		// split indexKeys in an array
 		$menuIndexKeys = explode(',', $this->conf['indexKeys']);
 
@@ -267,12 +366,28 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 		if ($this->conf['singlePid'] == '') return '<H1>'.$this->pi_getLL('errorSinglePid').'</H1>';
 
 		foreach ($menuIndexKeys as $indexKeys) {
-			if ($this->currentLetter == $indexKeys) {
-				$letter = $indexKeys;
+			if ($indexKeys == 'ALL') {
+				// define the all link
+				if ($this->currentLetter == 'ALL') {
+					$letter = $this->pi_getLL('allLable');
+				} else {
+					$letter = $this->pi_linkTP($this->pi_getLL('allLable'), array($this->prefixId.'[letter]' => $indexKeys), 1, $this->conf['singlePid']);
+				
+					// title parameter for link
+					$params = array('class' => 'tx-dgkeywordmenu-allLink');
+					$letter = $this->cObj->addParams($letter, $params);
+				}
+				
 			} else {
-				$letter = $this->pi_linkTP($indexKeys, array($this->prefixId.'[letter]' => $indexKeys), 1, $this->conf['singlePid']);
+				// define the other links
+				if ($this->currentLetter == $indexKeys) {
+					$letter = $indexKeys;
+				} else {
+					$letter = $this->pi_linkTP($indexKeys, array($this->prefixId.'[letter]' => $indexKeys), 1, $this->conf['singlePid']);
+				}
 			}
-			 
+
+			// Substitute marker
 			$subPartContent .= $this->cObj->substituteMarker($subTemplate, '###LETTER###', $letter);
 		}
 
@@ -280,6 +395,33 @@ class tx_dgkeywordmenu_pi1 extends tslib_pibase {
 		$content = $this->cObj->substituteSubpart($template, '###LETTERS###', $subPartContent);
 
 		return $content;
+	}
+	
+	
+	
+	/**
+	* Convert umlauts in a string for proper sorting in the list
+	*
+	* @param	string		$str: string to convert
+	* @return	string		Converted string (i.e. š => oe)
+	*/
+	function simplifyString($str) {
+
+		// Charset detection
+		if (isset($GLOBALS['TSFE']->renderCharset) && $GLOBALS['TSFE']->renderCharset != '') {
+			$charset = $GLOBALS['TSFE']->renderCharset;
+		} else {
+			$charset = 'iso-8859-1';
+		}
+
+		// Remove leading and trailing whitespace
+		$str = trim($str);
+
+		// Convert special chars using csConvObj
+		$str = $GLOBALS['TSFE']->csConvObj->conv_case($charset, $str, 'toLower');
+		$str = $GLOBALS['TSFE']->csConvObj->specCharsToASCII($charset, $str);
+		
+		return $str;
 	}
 	 
 	 
